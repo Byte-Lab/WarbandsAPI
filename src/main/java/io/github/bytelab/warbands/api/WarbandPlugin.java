10e06045b795e1c43c6603c62a3848557114bb6f
TBotV63 <tf2011-1@hotmail.de>
2014-10-28 21:37:56 +0100

    Replaced license message with gradle licenseFormat

--- src/main/java/io/github/bytelab/warbands/api/WarbandPlugin.java
+++ src/main/java/io/github/bytelab/warbands/api/WarbandPlugin.java
@@ -1,7 +1,10 @@
 /*
- * Copyright (C) 2014 Bytelab
+ * Copyright (c) 2014-2014
+ * Byte-Lab <http://bitcodermc.github.io/>
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of WarbandsAPI.
+ *
+ * WarbandsAPI is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -14,7 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package io.github.bytelab.warbands.api;
 
 public class WarbandPlugin {
